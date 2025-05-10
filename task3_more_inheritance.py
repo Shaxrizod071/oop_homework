7d37ebb9f47450faa900ae2b2d2e7dd81bf109d8
Shaxrizod071 <ahtamkulovshaxrizod@gmail.com>
2025-05-10 14:41:38 +0500

    sf

--- task3_more_inheritance.py
+++ task3_more_inheritance.py
@@ -13,3 +13,15 @@ Create another subclass called Teacher that inherits from Person:
 - Add a method teach() that prints "Teacher {name} is teaching {subject}."
 - Override the __str__ method to return "Teacher(name=name, age=age, subject=subject)"
 """
+class Teacher:
+  def __init__(self,name,age,subject):
+    self.name=name
+    self.age=age
+    self.subject=subject
+  def teach(self):
+    return f"Teacher{self.name} is teaching {self.subject}"
+  def __str__(self):
+    return f"Teacher{self.name,self.age,self.subject}"
+asd=Teacher("Samiya",34,"OOP")
+print(asd.teach())
+print(asd.__str__())
