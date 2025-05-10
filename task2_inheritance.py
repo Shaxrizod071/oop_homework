f2937d7833a68659724eb899821962ee48107a6f
Shaxrizod071 <ahtamkulovshaxrizod@gmail.com>
2025-05-10 14:28:28 +0500

    dsh

--- task2_inheritance.py
+++ task2_inheritance.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
 """
 📝 OOP Practice Homework - Task 2: Inherit from the Base Class
@@ -14,3 +12,15 @@ Create a subclass called Student that inherits from Person:
 - Override the __str__ method to include the grade
   (e.g., "Student(name=name, age=age, grade=grade)")
 """
+class Student:
+  def __init__(self,name,age,grade):
+     self.name=name
+     self.age=age
+     self.grade=grade
+  def study(self):
+    return (f"Hi {self.name} is studying")
+  def __str__(self):
+    return f"Hello {self.name} I am {self.age} years ago and I have got {self.grade}"
+asd=Student("Samae",21,5)
+print(asd.study())
+print(asd.__str__())
